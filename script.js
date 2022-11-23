a95477a99728ae87c8841d6df13358a2fe80d6da
shumailaaltaf <shumailaaltag34@gmail.com>
2022-11-23 15:09:06 +0500

    workin on else if

---
+++ script.js
@@ -0,0 +1,23 @@
+var day = "";
+
+if(day == "tuesday" ||day == "thursday"){
+    document.write('she eats cake on tuesday and thursday');
+}
+else if (day == "monday" ||day == "friday"){
+    document.write('she eats gurger');
+}
+else if (day == "wednesday"){
+    document.write('she drink coke');
+}
+else if(day == "monday" ||day == "tuesday", day == "wednesdays" ||day == "thursday"){
+    document.write('she eats bread');
+}
+else if(day == "friday" ||day == "saturday", day == "sunday"){
+    document.write('she eats bread');
+}
+else if (day == "saturday" ||day == "sunday"){
+    document.write('she take fruits');
+}
+else{
+    document.write('she always hungry');
+}
